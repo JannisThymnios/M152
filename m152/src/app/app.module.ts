c036f711375a07b6442db105459beeeeeb0badba
Jannis Thymnios <21m3616@bztf.ch>
2018-08-29 12:15:02 +0200

    Aufgaben Woche 3

--- m152/src/app/app.module.ts
+++ m152/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloworldComponent } from './helloworld/helloworld.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatModule } from './mat/mat.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { HelloworldComponent } from './helloworld/helloworld.component';
     HelloworldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ m152/src/app/mat/mat.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import {
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule
+ } from '@angular/material';
+
+
+
+@NgModule({
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class MatModule {}
